10d41ca0d110f3196d445ce06fc66f9dde653ab6
maksym-nenashev <nenashev@yahoo.com>
2022-04-21 22:52:44 +0200

    airport

--- app8.rb
+++ app8.rb
@@ -1,14 +1,14 @@
 class Country
     attr_reader :name,:airports
         
-    def initialize name
-      @name = neme
+    def initialize(name)
+      @name = name
       @airports = []
         end
     def add_airport airport
       @airports << airport
         end
-
+      end
 class Airport
     attr_reader :name,:planes
 
@@ -28,10 +28,14 @@ class Plane
          @model = model  
          end  
        end
-airports = []
+matrix = []
+#Sozdaem Country
+country = Country.new "USA"
+
 #Sozdaem Airport
-port1 = Airport.new "Domodedowo"
+port1 = Airport.new "Tokio"
 port2 = Airport.new "New York"
+country.add_airport port2 #Dobowlaem w stranu aeroport
 #Sozdaem samolety
 plane1 = Plane.new "Boing-747"
 plane2 = Plane.new "A-320" 
@@ -41,14 +45,14 @@ port1.add_plane plane2
 port2.add_plane plane1 #Port2
 port2.add_plane plane2
 
- airports << port1
- airports << port2
+matrix << country
+matrix << port2
 
- airports.each do |port|
+matrix.each do |port|
     puts "#{port.name}"
- port.plane.each do |plan|
-        puts "#{plan.model}"
-    end
+ #port2.plane.each do |plan|
+        ##puts "#{plan.model}"
+    #end
 end
 puts "Program finished. Press Enter to exit."
 gets
