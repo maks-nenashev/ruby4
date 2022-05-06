49480d586167119a3e4d72f66e17f711d0c2a16a
maksym-nenashev <nenashev@yahoo.com>
2022-05-06 05:52:50 +0200

    Modul

---
+++ app19.rb
@@ -0,0 +1,28 @@
+module Humans #Modul
+
+    class Animal
+    def run
+        puts"Running!"
+      end
+    def jump
+        puts"Jumping!"
+      end
+    end
+class Cat < Animal
+     def sey_meow
+        puts"Meeeeowwww!"
+       end
+      end
+class Dog < Animal
+     def bark
+      puts"Woof-Woof!"
+     end
+    end
+   end
+max = Humans::Dog.new
+max.bark
+
+
+puts "Program finished. Press Enter to exit."
+gets
+
