093a8354321d345411ecd403067cd10f80e0477b
maksym-nenashev <nenashev@yahoo.com>
2022-05-11 22:31:28 +0200

    send wyzow metoda po imeni

--- app26.rb
+++ app26.rb
@@ -1,12 +1,12 @@
-def aaa
-    puts"1st"
-   end
+def aaa        #def right
+    puts"1st"    #puts"Robot goes right"
+   end             #end
 
-def bbb
-    puts"2st"
-   end
+def bbb        #def left
+    puts"2st"    #puts"Robot goes left"
+   end             #end
 
-print"Enter method to call:"
+print"Enter method to call:" #print"Where to go "right/left""
 a = gets.strip
 
 send a #wyzow metoda po imeni
